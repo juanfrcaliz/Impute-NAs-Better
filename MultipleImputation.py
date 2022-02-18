905b31febeb9e30fbeadb6e557086c202f188bf2
Juanfrcaliz <juanfrcaliz@gmail.com>
2022-02-18 12:55:08 +0100

    Return regressors/classifiers for future use
    Deleted unused libraries
    
    Including regressors as a return value: useful for saving them in pickle and use in the future for imputing missing values on production data.

--- MultipleImputation.py
+++ MultipleImputation.py
@@ -1,36 +1,34 @@
-import random
 import math
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import BaggingRegressor
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import datasets
-import random
 import tqdm
 from sklearn.preprocessing import LabelEncoder
 
-def impute_missing_values(df,var_deviation_tolerance=0.97, actual_or_gaussian_residuals='actual', 
+def impute_missing_values(df,var_deviation_tolerance=0.97, actual_or_gaussian_residuals='actual',
                           col_floor_ceiling_dict=None, scores=False):
-    
+
     '''Impute missing values while minimizing distortion of variable distribution
     by creating a bagged model using other variables and adding residuals to output values
-    
+
     Parameters:
     df: dataframe with missing values
     var_deviation_tolerance: target percent deviation from original variable distributions
     actual_or_guassian_residuals: apply residuals to model outputs from actual distribution or from
         a gaussian distribution based on residuals' means and variances
-    col_floor_ceiling_dict: a dictionary with the variable name and a tuple of the min and max for variables 
+    col_floor_ceiling_dict: a dictionary with the variable name and a tuple of the min and max for variables
         with a finite range. Use float(inf) or float(-inf) for variables that are limited in only one direction
     scores: return accuracy score of models per variable
-    
+
     Returns:
     df: df with imputed values
     problems: columns that failed to impute
     column_scores: accuracy scores of imputation model on non-missing values
+    regressors: trained regressors that can be used for imputing missing values in future production data
     '''
+    regressors = [] # List that will contain the regressor trained for predicting values for each variable
+
     df = df.copy()
     columns = df.columns
     type_dict = df.dtypes.to_dict()
@@ -52,6 +50,7 @@ def impute_missing_values(df,var_deviation_tolerance=0.97, actual_or_gaussian_re
             X_have = int_df.loc[have_index,:]
             y_have = df[col][have_index]
             X_na = int_df.loc[na_index,:]
+
             if type_dict[col]=='object':
                 le = LabelEncoder()
                 y_have = le.fit_transform(y_have)
@@ -63,6 +62,7 @@ def impute_missing_values(df,var_deviation_tolerance=0.97, actual_or_gaussian_re
                 resid_preds = bagc.predict(X_have)
                 residuals = y_have-resid_preds
                 preds = bagc.predict(X_na)
+                regressors.append(bagc)
             else:
                 bagr = BaggingRegressor(n_estimators=m)
                 bagr.fit(X_have,y_have)
@@ -70,6 +70,7 @@ def impute_missing_values(df,var_deviation_tolerance=0.97, actual_or_gaussian_re
                 resid_preds = bagr.predict(X_have)
                 residuals = y_have-resid_preds
                 preds = bagr.predict(X_na)
+                regressors.append(bagr)
             if actual_or_gaussian_residuals=='actual':
                 rand_resids = np.random.choice(residuals,len(X_na),replace=True)
             else:
